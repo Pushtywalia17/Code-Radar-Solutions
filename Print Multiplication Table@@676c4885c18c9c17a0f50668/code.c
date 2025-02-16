032ae23bf4fef15d2799a7348bc65da62b8a0109
Pushtywalia17 <pushtywalia@gmail.com>
2025-02-16 22:38:56 +0530

    Solution to Print Multiplication Table

--- Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
+++ Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
@@ -3,10 +3,17 @@
 int main() {
     int num, limit;
 
+    // Taking input from the user
+    printf("Enter a number: ");
     scanf("%d", &num);
+
+    printf("Enter the limit: ");
     scanf("%d", &limit);
 
-    printf("%d\n", num);
+    // Printing the multiplication table header
+    printf("Multiplication Table of %d:\n", num);
+
+    // Loop to print the table
     for (int i = 1; i <= limit; i++) {
         printf("%d × %d = %d\n", num, i, num * i);
     }
