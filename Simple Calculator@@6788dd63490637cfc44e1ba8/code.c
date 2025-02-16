feacb8f4d58af38e671ab4ae6d3b1ee8a00c76d2
Pushtywalia17 <pushtywalia@gmail.com>
2025-02-16 15:54:26 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,22 +1,12 @@
 #include <stdio.h>
 int main() {
-    int num1, num2, sum;
-    scanf("%d %d", &num1, num2);
-    sum = (num1 + num2);
-        printf("%d\n", sum);
+    int num1, num2;
+    char op
+    scanf("%d %d %c", &num1, num2, op);
+    if (op== +) {
+        printf("%d\n", num1 + num2);
     }
-int main() {
-    int num1, num2, difference;
-    scanf("%d %d", &num1, num2);
-    difference = (num1 - num2);
-        printf("%d\n", difference);
-    }
-int main() {
-    int num1, num2, product;
-    scanf("%d %d", &num1, num2);
-    sum = (num1 * num2);
-        printf("%d\n", product);
-    }
+
     
     return 0;
     
