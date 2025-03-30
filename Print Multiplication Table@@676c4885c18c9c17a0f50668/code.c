5a0deae8f2b134f246705ba48de7805b223140a4
Pushtywalia17 <pushtywalia@gmail.com>
2025-03-30 17:00:52 +0530

    Solution to Print Multiplication Table

--- Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
+++ Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
-
 int main() {
-    int num, limit;
+    int num, range;
+    
+    // Input the number and range
+    scanf("%d %d", &num, &range);
 
-    // Taking input from the user
-    if (scanf("%d", &num) != 1 || scanf("%d", &limit) != 1) {
-        printf("Invalid input! Please enter integers only.\n");
-        return 1;  // Exit the program with an error code
-    }
-
-    // Printing the multiplication table
-    printf("Multiplication Table of %d:\n", num);
-    for (int i = 1; i <= limit; i++) {
-        printf("%d × %d = %d\n", num, i, num * i);
+    for (int i = 1; i <= range; i++) {    // Loop from 1 to the range
+        printf("%d x %d = %d\n", num, i, num * i);
     }
 
     return 0;
